aba43237b27e742a7d8ac66a1cc38eff2b5df4d1
zedroid2 <37702064+zedroid2@users.noreply.github.com>
2024-10-23 04:54:06 +0100

    potential fix to overflow error for tasks

--- js/empDashboard.js
+++ js/empDashboard.js
@@ -35,15 +35,26 @@ function getTasks() {
     let importanceColors = {1:"red", 2:"yellow", 3:"green"};
 
     tasks.forEach((task,index) => {
-        console.log(index);
-        tasksHTML.innerHTML += `
-        <div class="task" id="task${index}">
-            <div class="todo-task-importance">
-                <div class="todo-task-importance-inner" id="importance1" style="${importanceColors[task.importance]}"></div>
+        // detect overflow and add see more tasks button
+        // need to compare #tasks height(which has a predetermined)
+        // height of 0, to #todo-tasks wrapper
+        let tasksHeight = tasksHTML.clientHeight;
+        let todoTasksHeight = document.getElementById("todo-tasks");
+        if (tasksHeight+50 < todoTasksHeight) {
+            console.log(index);
+            tasksHTML.innerHTML += `
+            <div class="task" id="task${index}">
+                <div class="todo-task-importance">
+                    <div class="todo-task-importance-inner" id="importance1" style="${importanceColors[task.importance]}"></div>
+                </div>
+                <div class="todo-task-title">${task.title}</div>
+                <div class="todo-task-date">${task.due}</div>
             </div>
-            <div class="todo-task-title">${task.title}</div>
-            <div class="todo-task-date">${task.due}</div>
-        </div>
-        `
+            ` 
+        } else {
+            //else create see more tasks
+            document.getElementById("see-more-tasks").style.display="block;";
+        }
+        
     })
 }
