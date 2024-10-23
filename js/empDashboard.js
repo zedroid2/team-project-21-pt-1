58f863a56427a9fc6995c0d06bf4e141454d6d8a
zedroid2 <37702064+zedroid2@users.noreply.github.com>
2024-10-23 07:07:21 +0100

    task viewing, task completion fully functional

--- js/empDashboard.js
+++ js/empDashboard.js
@@ -24,17 +24,21 @@ function setNameAndDate() {
 
 function getTasks() {
     // get tasks as json string from local storage
-    let taskString = localStorage.getItem("user"); // CHANGE TO BE DYNAMIC
+    let currentUser = localStorage.getItem("current_user");
+    console.log
+    let taskString = localStorage.getItem(currentUser.substring(0,currentUser.indexOf("@"))); // CHANGE TO BE DYNAMIC
     // parse tasks to get json response
     let tasks = JSON.parse(taskString);
 
     // get the div where the tasks will be shown
-    tasksHTML = document.getElementById("tasks");
+    let tasksHTML = document.getElementById("tasks");
 
     // array for the colors of importance:
     let importanceColors = {1:"red", 2:"yellow", 3:"green"};
 
     console.log(tasks);
+    // remove all innerhtml to avoid misuse
+    tasksHTML.innerHTML = "";
 
     tasks.forEach((task,index) => {
         // detect overflow and add see more tasks button
@@ -43,10 +47,11 @@ function getTasks() {
         let tasksHeight = tasksHTML.clientHeight;
         let todoTasksHeight = document.getElementById("todo-tasks").clientHeight;
         console.log("tasksHeight: " + tasksHeight + ", todoTasksHeight: " + todoTasksHeight);
-        if (tasksHeight+50 < todoTasksHeight) {
+        // make sure the task is not yet completed too
+        if (tasksHeight+40 < todoTasksHeight*0.8 && task.active == true) {
             console.log(index);
             tasksHTML.innerHTML += `
-            <div class="task" id="task${index}">
+            <div class="task" id="task${index}" onclick="taskClick(event)">
                 <div class="todo-task-importance">
                     <div class="todo-task-importance-inner" id="importance1" style="${importanceColors[task.importance]}"></div>
                 </div>
@@ -57,8 +62,74 @@ function getTasks() {
         } else {
             //else create see more tasks
             console.log("else activated");
-            document.getElementById("see-more-tasks").style.display = "block;";
+            document.getElementById("see-more-tasks").innerHTML = "See More Tasks";
         }
         
     })
+}
+
+
+function taskClick(event) {
+    let task = event.target.parentElement;
+    console.log(task);
+    // Displaying info to the right of the task list
+    let detailedTask = document.getElementById("detailed-task");
+    // clear the placeholder text:
+  let temp = document.getElementById("detailed-task-placeholder");
+  console.log(temp);
+    document.getElementById("detailed-task-placeholder").style.display = "none";
+
+    // make the detailed info visible:
+    document.getElementById("detailed-task-inner").display = "block";
+
+    // get all the elements to change
+    /*let title = document.getElementById("detailed-task-title");
+    let description = document.getElementById("detailed-task-description");
+    let due = document.getElementById("detailed-task-due");
+    let status = document.getElementById("detailed-task-status");
+    let assigner = document.getElementById("detailed-task-aligner");
+    let completed = document.getElementById("detailed-task-complete");
+*/
+    // get individual task details:
+    let taskIndex = parseInt(task.id.substring(4))
+    // * get the tasks from local storage
+    let currentUser = localStorage.getItem("current_user");
+    let tasks = JSON.parse(localStorage.getItem(currentUser.substring(0,currentUser.indexOf("@"))));
+    let wantedTask = tasks[taskIndex];
+    console.log(wantedTask);
+
+    document.getElementById("detailed-task-inner").innerHTML = `
+    <div id="detailed-task-inner">
+        <div id="detailed-task-title">${wantedTask.title}</div>
+        <div id="detailed-task-description">${wantedTask.description}</div>
+        <div id="detailed-task-due">Due : <span>${wantedTask.due}</span></div>
+        <div id="detailed-task-status">Status : <span>${wantedTask.active}</span></div>
+        <div id="detailed-task-assigner">Assigner : <span>${wantedTask.assigner}</span></div>
+        <div class="detailed-task-complete" id="detailed-task-complete${taskIndex}" onclick="taskCompleted(event)">Completed?</div>
+    </div>
+    `
+
+    /*title.innerHTML = wantedTask.title;
+    description.innerHTML = wantedTask.description;
+    due.innerHTML = wantedTask.due;
+    status.innerHTML = wantedTask.active;
+    assigner.innerHTML = wantedTask.assigner;*/
+}
+
+function taskCompleted(event) {
+    console.log(event.target);
+    // make button green
+    event.target.style.background = "green";
+    // we want to set the task as completed so:
+    let taskIndex = event.target.id.match(/(\d+)/)[0];
+    let currentUser = localStorage.getItem("current_user");
+    let tasks = JSON.parse(localStorage.getItem(currentUser.substring(0,currentUser.indexOf("@"))));
+    console.log(taskIndex);
+    // change the task active at the taskIndex:
+    tasks[taskIndex].active = false;
+    // set to local storage
+    localStorage.setItem(currentUser.substring(0,currentUser.indexOf("@")), JSON.stringify(tasks));
+    // window reload
+    location.reload();
+  
 }
--- js/index.js
+++ js/index.js
@@ -1,96 +1,152 @@
 const emails = {
-        "abhay@make-it-all.co.uk":"#Abhay123",
-        "charlie@make-it-all.co.uk":"#Charlie123",
-        "christo@make-it-all.co.uk":"#Christo123"
-    };
+    "abhay@make-it-all.co.uk":"#Abhay123",
+    "charlie@make-it-all.co.uk":"#Charlie123",
+    "christo@make-it-all.co.uk":"#Christo123"
+};
 localStorage.setItem("abhay@make-it-all.co.uk", JSON.stringify(["#Abhay123", "Employee"]));
 localStorage.setItem("charlie@make-it-all.co.uk", JSON.stringify(["#Charlie123", "Employee"]))
 localStorage.setItem("christo@make-it-all.co.uk", JSON.stringify(["#Christo123", "Employee"]));
 
 //to do tasks: Title, importance, description, 
 let tasks1 = [
-    {
-        title: "Task 1",
-        importance: 1,
-        description: "A detailed description of task 1 and its various requirements",
-        due: "22/10/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 2",
-        importance: 1,
-        description: "A detailed description of task 2 and its various requirements",
-        due: "26/10/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 3",
-        importance: 2,
-        description: "A detailed description of task 3 and its various requirements",
-        due: "29/10/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 4",
-        importance: 2,
-        description: "A detailed description of task 4 and its various requirements",
-        due: "02/11/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 5",
-        importance: 3,
-        description: "A detailed description of task 5 and its various requirements",
-        due: "17/12/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 6",
-        importance: 3,
-        description: "A detailed description of task 6 and its various requirements",
-        due: "26/10/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    },
-    {
-        title: "Task 7",
-        importance: 3,
-        description: "A detailed description of task 7 and its various requirements",
-        due: "26/10/24",
-        active: true,
-        assigner: "Charlie Wyke"
-    }
+{
+    title: "Task 1",
+    importance: 1,
+    description: "A detailed description of task 1 and its various requirements",
+    due: "22/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 2",
+    importance: 1,
+    description: "A detailed description of task 2 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 3",
+    importance: 2,
+    description: "A detailed description of task 3 and its various requirements",
+    due: "29/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 4",
+    importance: 2,
+    description: "A detailed description of task 4 and its various requirements",
+    due: "02/11/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 5",
+    importance: 3,
+    description: "A detailed description of task 5 and its various requirements",
+    due: "17/12/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 6",
+    importance: 3,
+    description: "A detailed description of task 6 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 7",
+    importance: 3,
+    description: "A detailed description of task 7 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 8",
+    importance: 1,
+    description: "A detailed description of task 8 and its various requirements",
+    due: "22/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 9",
+    importance: 1,
+    description: "A detailed description of task 9 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 10",
+    importance: 2,
+    description: "A detailed description of task 10 and its various requirements",
+    due: "29/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 11",
+    importance: 2,
+    description: "A detailed description of task 11 and its various requirements",
+    due: "02/11/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 12",
+    importance: 3,
+    description: "A detailed description of task 12 and its various requirements",
+    due: "17/12/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 13",
+    importance: 3,
+    description: "A detailed description of task 13 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+},
+{
+    title: "Task 14",
+    importance: 3,
+    description: "A detailed description of task 14 and its various requirements",
+    due: "26/10/24",
+    active: true,
+    assigner: "Charlie Wyke"
+}
 ]
 // assign tasks to local storage
-localStorage.setItem("user", JSON.stringify(tasks1));
+localStorage.setItem("abhay", JSON.stringify(tasks1));
 
 
 function submitEmail() {
-    // get email entered
-    let email = document.getElementById("email").value;
+// get email entered
+let email = document.getElementById("email").value;
 
-    if (localStorage.getItem(email) != null) {
-        // make the password visible
-        document.getElementById("password-container").style.display = "block";
-    }
+if (localStorage.getItem(email) != null) {
+    // make the password visible
+    document.getElementById("password-container").style.display = "block";
+}
 }
 
 function login(event) {
-    event.preventDefault();
+event.preventDefault();
 
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
 
-    if (emails[email] === password) {
-        localStorage.setItem("current_user", email);
-        console.log(localStorage.getItem("current_user"))
-        window.location.replace("empDashboard.html");
-    } else {
-        console.log("incorrect login");
-    }
+if (emails[email] === password) {
+    localStorage.setItem("current_user", email);
+    console.log(localStorage.getItem("current_user"))
+    window.location.replace("empDashboard.html");
+} else {
+    console.log("incorrect login");
+}
 }
