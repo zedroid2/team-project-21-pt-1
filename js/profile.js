d703e486d246ab5ec83525e3ae31b09ed4ec5bf0
abhay0204 <84336764+abhay0204@users.noreply.github.com>
2024-11-03 02:19:46 +0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -25,6 +25,23 @@ function changePassword() {
     alert("Password change functionality not yet implemented.");
 }
 
+function logout() {
+    window.location.href = 'index.html';
+}
+
+// Functions for sidebar
+function openProfile() {
+    window.location.href = 'profile.html';
+}
+  
+function openTraining() {
+    window.location.href = 'training.html';
+}
+  
 function openForum() {
     window.location.href = 'forum.html';
+}
+
+function openCalendar() {
+    window.location.href = 'calendar.html';
 }
