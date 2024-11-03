a015b25ee082133c2b421b974202b3567b25ad3e
zedroid2 <37702064+zedroid2@users.noreply.github.com>
2024-11-03 19:40:46 +0000

    Create sidebar.js

---
+++ js/sidebar.js
@@ -0,0 +1,16 @@
+// Functions for sidebar
+function openProfile() {
+    window.location.href = 'profile.html';
+}
+  
+function openTraining() {
+    window.location.href = 'training.html';
+}
+  
+function openForum() {
+    window.location.href = 'forum.html';
+}
+
+function openCalendar() {
+    window.location.href = 'calendar.html';
+}
